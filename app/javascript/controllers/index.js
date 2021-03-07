2b04ad2be2efd873517e2c9341200113a6755979
Nolan Tait <nolanjtait@gmail.com>
2021-03-07 00:03:01 -0800

    Adds style for wizards

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
 import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
-import { Dropdown, Pagination } from 'ui-reflex'
+import { Dropdown, Pagination, Wizard } from 'ui-reflex'
 
 const application = Application.start()
 const context = require.context('.', true, /.js$/)
@@ -9,3 +9,4 @@ application.load(definitionsFromContext(context))
 
 application.register('dropdown', Dropdown)
 application.register('pagination', Pagination)
+application.register('wizard', Wizard)
