ca0c1ecf961bc9d8db418d9ea2403536d171fbbe
Nolan Tait <nolanjtait@gmail.com>
2021-03-09 00:00:35 -0800

    Adds tables

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
 import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
-import { Dropdown, Pagination, Wizard } from 'ui-reflex'
+import { Dropdown, Pagination, Wizard, Selectable, Sortable } from 'ui-reflex'
 
 const application = Application.start()
 const context = require.context('.', true, /.js$/)
@@ -10,3 +10,5 @@ application.load(definitionsFromContext(context))
 application.register('dropdown', Dropdown)
 application.register('pagination', Pagination)
 application.register('wizard', Wizard)
+application.register('selectable', Selectable)
+application.register('sortable', Sortable)
